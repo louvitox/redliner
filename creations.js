5beb202f5ba9ccdc8718b3b96bfe6c01d8234083
louvitox <louvitox2.0@gmail.com>
2026-06-14 00:21:50 +0200

    Add files via upload

---
+++ creations.js
@@ -0,0 +1,23 @@
+
+
+const CREATIONS = [
+
+  {
+    image: "https://media.discordapp.net/attachments/1515357896202915951/1515357898253926440/IMG_1501.png?ex=6a2eb684&is=6a2d6504&hm=8d4d94703a569ca10e0294fc85f1fb3d032de30fc45723e3bea542e5bee12d85&=&format=webp&quality=lossless&width=1872&height=819",
+    author: "Zurina",
+    link: "https://discord.com/channels/1399758449423810703/1515357896202915951"
+  },
+
+  {
+    image: "https://media.discordapp.net/attachments/1468865121479561298/1471084804731568223/Untitled16_20260211000428.png?ex=6a2f25bc&is=6a2dd43c&hm=b5d44cd5f2039b08bc8e0c23569f11c0c062371b052ae8acf1c42319342e43f7&=&format=webp&quality=lossless&width=1522&height=904",
+    author: "Alphagaster",
+    link: "https://discord.com/channels/1399758449423810703/1468865121479561298"
+  },
+
+  {
+    image: "https://media.discordapp.net/attachments/1512970096740663537/1512970096937537716/redlinerart2.png?ex=6a2e9874&is=6a2d46f4&hm=116f7cf88e5dcc9f3aac46acce8fb74ba5f8608076d163e4cae2e9c66402dbaa&=&format=webp&quality=lossless",
+    author: "Kubare",
+    link: "https://discord.com/channels/1399758449423810703/1512970096740663537"
+  },
+
+];
