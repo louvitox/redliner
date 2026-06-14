5511e83465b5ebd60e5a98ed5db3b8a5b58476c7
louvitox <louvitox2.0@gmail.com>
2026-06-14 11:11:17 +0200

    Update clips.js

--- clips.js
+++ clips.js
@@ -1,14 +1,10 @@
-
-
 const CLIPS = [
 
- 
   {
-    type: "Vidéo",
-    id: "RedLiner-timer.mp4",
-    title: "Timer June 13 16:00 EST ",
+    type: "video",
+    url: "RedLiner-timer.mp4",
+    title: "Timer June 13 16:00 EST",
     author: "Louvitox"
   },
 
- 
 ];
