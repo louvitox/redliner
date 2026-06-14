c9f998349f79295cb8683a50e670965bea01d731
louvitox <louvitox2.0@gmail.com>
2026-06-14 11:06:07 +0200

    Update clips.js

--- clips.js
+++ clips.js
@@ -5,26 +5,10 @@ const CLIPS = [
  
   {
     type: "Vidéo",
-    id: "Minuteur RedLiner.mp4",
+    id: "RedLiner-timer.mp4",
     title: "Timer June 13 16:00 EST ",
     author: "Louvitox"
   },
 
-  // --- Exemple embed (Streamable / Medal...) ---
-  // {
-  //   type: "embed",
-  //   url: "https://streamable.com/e/XXXXXX",
-  //   title: "Clutch 1v4",
-  //   author: "PseudoDiscord"
-  // },
-
-  // --- Exemple lien simple avec vignette ---
-  // {
-  //   type: "link",
-  //   url: "https://medal.tv/...",
-  //   thumb: "https://...image.png",
-  //   title: "Gros frag",
-  //   author: "PseudoDiscord"
-  // },
-
+ 
 ];
